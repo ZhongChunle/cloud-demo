31c64f47f2fd16f3fafb90331ac1ff18f0e590ce
zhongcle@outlook.com <zhongcle@outlook.com>
2022-05-28 10:10:24 +0800

    eureka服务注册，服务拉取，负载均衡分配user-server请求地址

---
+++ eureka-server2/src/main/java/com/zcl/Application.java
@@ -0,0 +1,20 @@
+package com.zcl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 项目名称：cloud-demo
+ * 描述：引导类
+ *
+ * @author zhong
+ * @date 2022-05-27 7:05
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
